83c336a93385a7656abedeeb22818dead0f5295b
Danil Buslaev <buslaev_d@garpix.com>
2024-01-30 20:58:47 +0300

    [new] project init

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  printWidth: 100,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  arrowParens: 'always',
+  bracketSpacing: true,
+}
